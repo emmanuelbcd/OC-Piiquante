eba6b229c6942307edf2a54c8d176f7dc71db593
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-07-26 07:01:01 +0200

    user model

---
+++ back/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+//on ajoute mongoose-unique-validator comme plugin à notre schéma
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+    //avec unique = true, impossible de s'inscrire plusieurs fois avec la même adresse mail
+    email: {type: String, required: true, unique: true}, 
+    password : {type: String, required: true} //le mot de passe crypté est de type string
+});
+
+userSchema.plugin(uniqueValidator);
+
+//on exporte userSchema sous forme de modèle
+module.exports = mongoose.model('User', userSchema);
