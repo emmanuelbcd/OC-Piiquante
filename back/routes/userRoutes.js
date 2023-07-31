cdf54be6c5942652c789c597c3cacf6e22f1706c
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-07-31 22:18:18 +0200

    update

--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router(); // création du router d'express
 const userCtrl = require('../controllers/userCtrl'); // création du contrôleur pour associer les fonctions aux différentes routes
 
 //routes POST car le front va envoyer des infos (email et mot de passe)
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', userCtrl.signup); //création d'un compte utilisateur
+router.post('/login', userCtrl.login); //connexion
 
 module.exports = router; // exportation du router
