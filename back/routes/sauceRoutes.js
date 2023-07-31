87d76b9e10f4d3468ef56675bbef518faf14d2fa
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-07-31 22:17:28 +0200

    update

--- back/routes/sauceRoutes.js
+++ back/routes/sauceRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const Thing = require('../models/Sauce');
-
-
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
