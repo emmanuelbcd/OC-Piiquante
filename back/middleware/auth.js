e3ff810d9876c3d579440cbcc238d73e152c239f
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-08-05 09:30:57 +0200

    log error

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -1,15 +1,21 @@
-const jwt = require('jsonwebtoken'); //importation de jsonwebtoken
+const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1]; // on récupère notre token
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // on décode notre token avec la méthode verify
-        const userId = decodedToken.userId; // on récupère le userId
+        if (!req.headers.authorization) {
+            return res.status(401).json({ message: 'Authorization token missing!' });
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodedToken.userId;
         req.auth = {
             userId: userId
         };
         next();
     } catch(error) {
+        console.log(error);
         res.status(401).json( { message: 'Une erreur est survenue !' } );
     }
 };
+
+
