c40d7d861889f025ab4922abd499b2e3b5bd26e4
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-08-05 09:28:15 +0200

    sauce route update

--- back/routes/sauceRoutes.js
+++ back/routes/sauceRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const auth = require('../middleware/auth');
-const multer = require('../middleware/multer-config');
+const auth = require('../middleware/auth'); //middleware d'authentification
+const multer = require('../middleware/multer-config'); //middleware de gestion de fichiers
 
 const sauceCtrl = require('../controllers/sauceCtrl');
 
+// on définit les routes pour chaque opération CRUD
+//pour chaque route, on applique le middleware d'authentification pour protéger les routes
+//pour les routes qui ont besoin de traiter les images de sauces, on applique le middleware multer
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.post('/', auth, multer, sauceCtrl.createSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
