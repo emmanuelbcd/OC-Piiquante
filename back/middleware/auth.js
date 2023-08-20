34ee75ecb1dc14f2f7387e1f355ae5f0793dc51f
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-08-20 22:28:00 +0200

    comments

--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -1,3 +1,11 @@
+//l'objectif de ce middleware est de vérifier que la requête à l'API est bien authentifiée 
+// à l'aide du token JWT JSON Web Token
+
+//Lorsqu'un utilisateur essaie d'accéder à certaines routes qui nécessitent une authentification,
+//la requête passe par ce middleware.
+//si l'utilisateur est authentifié (c-à-d qu'il a fourni un token valide), il peut continuer.
+//sinon, il reçoit une réponse d'erreur.
+
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
@@ -6,7 +14,8 @@ module.exports = (req, res, next) => {
             return res.status(401).json({ message: 'Authorization token missing!' });
         }
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const jwtSecret = process.env.JWT_SECRET;
+        const decodedToken = jwt.verify(token, jwtSecret);
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
