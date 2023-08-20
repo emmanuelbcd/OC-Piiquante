a75fc908968da18e4ab535a44b4e7e5495cf82c8
Emmanuel Blanchard <emmanuel.blanchard@outlook.com>
2023-08-20 21:39:40 +0200

    comments

--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -1,3 +1,8 @@
+//une route est une méthode HTTP (GET, POST, PUT, DELETE, etc.) et
+//une route est un chemin (par exemple, '/signup')
+//routage : lorsqu'une requête atteint le serveur, elle est dirigée vers la route correspondante
+//basée sur la méthode et le chemin
+
 const express = require('express'); //on a besoin d'express afin de créer un router
 const router = express.Router(); // création du router d'express
 const userCtrl = require('../controllers/userCtrl'); // création du contrôleur pour associer les fonctions aux différentes routes
